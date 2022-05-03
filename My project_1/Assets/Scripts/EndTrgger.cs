04d5822460e21ea6c03fc219a8a3c5488a74b3a4
KaterynaMaslo <kateryna.vasylkova@stud.onu.edu.ua>
2022-05-03 03:33:25 +0300

    init

---
+++ My project_1/Assets/Scripts/EndTrgger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EndTrgger : MonoBehaviour
+{
+    public GameManager gameManager;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Win"))
+        {
+            gameManager.CompleteLevel();
+        }
+    }
+}
---
+++ My project_1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject completeLevelUI;
+
+    bool gameHasEnded = false;
+
+    public float restartDelay;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded ==false)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
